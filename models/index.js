3a36c62e480c06b470494587f283e8d0428d854e
David Paucar <soydavidpaucar@gmail.com>
2022-07-13 16:06:41 -0500

    :technologist: exported all models in a single file

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const models = {
+	UserModel: require('./nosql/users.model'),
+	TrackModel: require('./nosql/tracks.model'),
+	StorageModel: require('./nosql/storage.model')
+};
+
+module.exports = models;
