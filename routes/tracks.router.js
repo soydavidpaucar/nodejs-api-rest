12dbe124c9853736e5153b431cb84100dc832b40
David Paucar <soydavidpaucar@gmail.com>
2022-07-13 16:07:44 -0500

    :necktie: added create track controller logic to the router

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getTracks } = require('../controllers/tracks.controller');
+const { getTracks, createTrack } = require('../controllers/tracks.controller');
 const router = express.Router();
 
 router.get('/', getTracks);
+router.post('/', createTrack);
 
 module.exports = router;
