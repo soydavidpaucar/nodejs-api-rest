00216eb6389b39a246b42abd97d5a7c4f583d75d
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 15:26:52 -0500

    :necktie: created track validator schema with joi

---
+++ validators/tracks.validator.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const validatorCreateTrack = Joi.object().keys({
+	name: Joi.string().required(),
+	album: Joi.string().required(),
+	cover: Joi.string().required(),
+	artist: {
+		name: Joi.string().required(),
+		nickname: Joi.string().required(),
+		nationality: Joi.string().required()
+	},
+	duration: {
+		start: Joi.number().required(),
+		end: Joi.number().required()
+	},
+	mediaId: Joi.string().hex().length(24)
+});
+
+module.exports = validatorCreateTrack;
