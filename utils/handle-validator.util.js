aa9cbfb4a11c79bd176f67e9d47ff8a3132e3022
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 18:12:26 -0500

    :art: validated and normalized body to remove unknown keys

--- utils/handle-validator.util.js
+++ utils/handle-validator.util.js
@@ -1,15 +1,22 @@
 const validator = (schema) => (request, response, next) => {
 	const { error, value } = schema.validate(request.body, {
 		abortEarly: false,
+		stripUnknown: {
+			objects: true,
+			arrays: true
+		},
+		convert: false,
 		errors: {
 			wrap: {
 				label: ''
 			}
 		}
 	});
+
 	if (error) {
 		return response.status(400).json({ status: 400, message: error.details.map((e) => e.message), error: 'Bad Request' });
 	}
+	request.body = value;
 	return next();
 };
 
