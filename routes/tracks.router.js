c5d80815dafcac0268d520b6817c3e73df4db034
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 18:34:52 -0500

    :art: added route get track by id with its handler and controller

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getTracks, createTrack } = require('../controllers/tracks.controller');
+const { getTracks, createTrack, getTrackById } = require('../controllers/tracks.controller');
 const validator = require('../utils/handle-validator.util');
-const { validatorCreateTrack } = require('../validators/tracks.validator');
+const { validatorCreateTrack, validatorGetTrackById } = require('../validators/tracks.validator');
 const router = express.Router();
 
 router.get('/', getTracks);
+router.get('/:id', validator(validatorGetTrackById), getTrackById);
 router.post('/', validator(validatorCreateTrack), createTrack);
 
 module.exports = router;
