cc7cc5a758b95ec639c9cbd03448d7051844ae4f
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 15:28:12 -0500

    :technologist: added validator middleware to post track request

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getTracks, createTrack } = require('../controllers/tracks.controller');
+const validator = require('../utils/handle-validator.util');
+const validatorCreateTrack = require('../validators/tracks.validator');
 const router = express.Router();
 
 router.get('/', getTracks);
-router.post('/', createTrack);
+router.post('/', validator(validatorCreateTrack), createTrack);
 
 module.exports = router;
