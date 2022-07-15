518d29e32a82c36805155995d9d680b0b3e390a7
David Paucar <soydavidpaucar@gmail.com>
2022-07-15 16:23:08 -0500

    :art: added storage update route

--- routes/storage.router.js
+++ routes/storage.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createStorage, getStorages, deleteStorage, getStorageById } = require('../controllers/storage.controller');
+const { createStorage, getStorages, deleteStorage, getStorageById, updateStorage } = require('../controllers/storage.controller');
 const uploadMiddleware = require('../utils/handle-storage.util');
 const { validatorParamId } = require('../utils/handle-validator.util');
 const { validatorGetStorageById } = require('../validators/storage.validator');
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', getStorages);
 router.get('/:id', validatorParamId(validatorGetStorageById), getStorageById);
 router.post('/', uploadMiddleware.single('myfile'), createStorage);
+router.put('/:id', uploadMiddleware.single('myfile'), validatorParamId(validatorGetStorageById), updateStorage);
 router.delete('/:id', validatorParamId(validatorGetStorageById), deleteStorage);
 
 module.exports = router;
