3d0ff02f064a0ec626e9d52f257ef72aad83dd5a
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 23:52:19 -0500

    :art: improved error handling and created validator for param id requests

--- utils/handle-validator.util.js
+++ utils/handle-validator.util.js
@@ -13,11 +13,33 @@ const validator = (schema) => (request, response, next) => {
 		}
 	});
 	if (error) {
-		response.status(400).send({ error: error.details.map(({ message }) => message) });
+		return response.status(400).json({ status: 400, message: error.details.map((e) => e.message), error: 'Bad Request' });
 	} else {
 		request.body = value;
-		next();
+		return next();
 	}
 };
 
-module.exports = validator;
+const validatorParamId = (schema) => (request, response, next) => {
+	const { error, value } = schema.validate(request.params, {
+		abortEarly: false,
+		stripUnknown: {
+			objects: true,
+			arrays: true
+		},
+		convert: false,
+		errors: {
+			wrap: {
+				label: ''
+			}
+		}
+	});
+	if (error) {
+		return response.status(400).json({ status: 400, message: error.details.map((e) => e.message), error: 'Bad Request' });
+	} else {
+		request.params = value;
+		return next();
+	}
+};
+
+module.exports = { validator, validatorParamId };
