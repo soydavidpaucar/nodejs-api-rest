f2d223786bb24b09f6b17a794a48a2107ca84e67
David Paucar <soydavidpaucar@gmail.com>
2022-07-14 23:52:50 -0500

    :art: imported validator to handle its correct schema

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getTracks, createTrack, getTrackById } = require('../controllers/tracks.controller');
-const validator = require('../utils/handle-validator.util');
+const { getTracks, createTrack, getTrackById, updateTrack } = require('../controllers/tracks.controller');
+const { validator, validatorParamId } = require('../utils/handle-validator.util');
 const { validatorCreateTrack, validatorGetTrackById } = require('../validators/tracks.validator');
 const router = express.Router();
 
 router.get('/', getTracks);
-router.get('/:id', validator(validatorGetTrackById), getTrackById);
+router.get('/:id', validatorParamId(validatorGetTrackById), getTrackById);
 router.post('/', validator(validatorCreateTrack), createTrack);
+router.put('/:id', validator(validatorCreateTrack), validatorParamId(validatorGetTrackById), updateTrack);
 
 module.exports = router;
