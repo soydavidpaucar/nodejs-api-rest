d2583b6a8d09eee4746a99d36610777e1d90c610
David Paucar <soydavidpaucar@gmail.com>
2022-07-15 15:29:59 -0500

    :necktie: added storage routes and validator middleware to handle id param

--- routes/storage.router.js
+++ routes/storage.router.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { createStorage, getStorages } = require('../controllers/storage.controller');
+const { createStorage, getStorages, deleteStorage, getStorageById } = require('../controllers/storage.controller');
 const uploadMiddleware = require('../utils/handle-storage.util');
+const { validatorParamId } = require('../utils/handle-validator.util');
+const { validatorGetStorageById } = require('../validators/storage.validator');
 const router = express.Router();
 
 router.get('/', getStorages);
+router.get('/:id', validatorParamId(validatorGetStorageById), getStorageById);
 router.post('/', uploadMiddleware.single('myfile'), createStorage);
+router.delete('/:id', validatorParamId(validatorGetStorageById), deleteStorage);
 
 module.exports = router;
