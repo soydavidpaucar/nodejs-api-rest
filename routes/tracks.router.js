b52eb3613113d5eb4c15040d8cdd5bef862cac0e
David Paucar <soydavidpaucar@gmail.com>
2022-07-15 21:49:31 -0500

    :art: imported middleware to handle authorization requests

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getTracks, createTrack, getTrackById, updateTrack, deleteTrack } = require('../controllers/tracks.controller');
+const sessionMiddleware = require('../middlewares/session.middleware');
 const { validator, validatorParamId } = require('../utils/handle-validator.util');
 const { validatorCreateTrack, validatorGetTrackById } = require('../validators/tracks.validator');
 const router = express.Router();
 
-router.get('/', getTracks);
+router.get('/', sessionMiddleware, getTracks);
 router.get('/:id', validatorParamId(validatorGetTrackById), getTrackById);
 router.post('/', validator(validatorCreateTrack), createTrack);
 router.put('/:id', validator(validatorCreateTrack), validatorParamId(validatorGetTrackById), updateTrack);
