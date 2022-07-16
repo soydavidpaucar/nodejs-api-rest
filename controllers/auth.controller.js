72eafbd5945b6aa9df923829ccc582ce75962d38
David Paucar <soydavidpaucar@gmail.com>
2022-07-15 21:10:50 -0500

    :necktie: created logic to login an user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const usersModel = require('../models/nosql/users.model');
 const { signToken } = require('../utils/handle-jwt.util');
-const { encryptPassword } = require('../utils/handle-password.util');
+const { encryptPassword, comparePassword } = require('../utils/handle-password.util');
 
 const registerUser = async (request, response) => {
 	const { password } = request.body;
@@ -18,4 +18,28 @@ const registerUser = async (request, response) => {
 	return response.send(data);
 };
 
-module.exports = { registerUser };
+const loginUser = async (request, response) => {
+	const user = await usersModel.findOne({ email: request.body.email }).select('+password');
+
+	if (!user) {
+		return response.status(401).send({ message: 'User not found' });
+	}
+	const hashPassword = user.password;
+
+	const comparePasswords = await comparePassword(request.body.password, hashPassword);
+
+	if (!comparePasswords) {
+		return response.status(401).send({ message: 'Password incorrect' });
+	}
+
+	user.set('password', undefined);
+
+	const data = {
+		token: signToken(user),
+		user: user
+	};
+
+	return response.send(data);
+};
+
+module.exports = { registerUser, loginUser };
