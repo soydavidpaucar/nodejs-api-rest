8a2f6e29b68532344a9ceae91d44e47693668f3a
David Paucar <soydavidpaucar@gmail.com>
2022-07-15 21:11:22 -0500

    :art: imported login route with its middleware and controller

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { validatorRegisterUser } = require('../validators/auth.validator');
+const { validatorRegisterUser, validatorLoginUser } = require('../validators/auth.validator');
 const { validator } = require('../utils/handle-validator.util');
-const { registerUser } = require('../controllers/auth.controller');
+const { registerUser, loginUser } = require('../controllers/auth.controller');
 
 router.post('/register', validator(validatorRegisterUser), registerUser);
+router.post('/login', validator(validatorLoginUser), loginUser);
 
 module.exports = router;
