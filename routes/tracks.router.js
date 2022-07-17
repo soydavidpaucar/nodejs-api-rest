10a6e44edec48f9b77e4aab23a422aa4d6fb4034
David Paucar <soydavidpaucar@gmail.com>
2022-07-17 18:16:05 -0500

    :lock: added middleware to protect routes by user role

--- routes/tracks.router.js
+++ routes/tracks.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { getTracks, createTrack, getTrackById, updateTrack, deleteTrack } = require('../controllers/tracks.controller');
+const checkRole = require('../middlewares/role.middleware');
 const sessionMiddleware = require('../middlewares/session.middleware');
 const { validator, validatorParamId } = require('../utils/handle-validator.util');
 const { validatorCreateTrack, validatorGetTrackById } = require('../validators/tracks.validator');
@@ -7,7 +8,7 @@ const router = express.Router();
 
 router.get('/', sessionMiddleware, getTracks);
 router.get('/:id', sessionMiddleware, validatorParamId(validatorGetTrackById), getTrackById);
-router.post('/', sessionMiddleware, validator(validatorCreateTrack), createTrack);
+router.post('/', sessionMiddleware, checkRole(['admin']), validator(validatorCreateTrack), createTrack);
 router.put('/:id', sessionMiddleware, validator(validatorCreateTrack), validatorParamId(validatorGetTrackById), updateTrack);
 router.delete('/:id', sessionMiddleware, validatorParamId(validatorGetTrackById), deleteTrack);
 
